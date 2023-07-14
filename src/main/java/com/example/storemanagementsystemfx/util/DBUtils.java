d8a2065bb661742bbaff2a2b2b94afd5e67b508c
LuanTang <luantang.work@gmail.com>
2023-07-14 14:23:02 +0700

    change dbutils connection

--- src/main/java/com/example/storemanagementsystemfx/util/DBUtils.java
+++ src/main/java/com/example/storemanagementsystemfx/util/DBUtils.java
@@ -3,14 +3,10 @@ package com.example.storemanagementsystemfx.util;
 import java.sql.*;
 
 public class DBUtils {
-//    private static String url = "jdbc:mysql://sql6.freemysqlhosting.net/sql6632248";
-//    private static String username = "sql6632248";
-//    private static String password = "35CqJVLsnr";
-    private static String url = "jdbc:mysql://localhost:3306/storems-fx";
-    private static String username = "root";
-    private static String password = "Khung1235!";
+    private static String url = "jdbc:mysql://sql6.freemysqlhosting.net/sql6632248";
+    private static String username = "sql6632248";
+    private static String password = "35CqJVLsnr";
     private static String driver = "com.mysql.cj.jdbc.Driver";
-
 
     public static Connection getConnection() throws Exception {
         Class.forName(driver);
